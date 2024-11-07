8f97fa9b47f9b7b6d12cd3ef682466677aae983d
HyeonjunSon <son7523589@gmail.com>
2024-11-07 14:46:14 -0500

    edit api setting

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-// server/app.js
+// server/api/projects.js
 const express = require('express');
 const path = require('path');
-const projectsRoute = require('./routes/projects');
+const projectsRoute = require('../routes/projects'); // 상대 경로 수정
 require('dotenv').config();
 
 const app = express();
@@ -10,14 +10,12 @@ const app = express();
 app.use('/api/projects', projectsRoute);
 
 // 프론트엔드 빌드 파일 제공
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 // 모든 기타 요청에 대해 React의 index.html 반환 (클라이언트 측 라우팅 지원)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3600;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen() 생략: Vercel에서 서버리스 함수로 실행
+module.exports = app;
