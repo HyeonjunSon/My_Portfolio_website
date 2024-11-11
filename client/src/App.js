60a14f41350d09786882845ae501225cd407ca1a
HyeonjunSon <son7523589@gmail.com>
2024-11-11 18:08:47 -0500

    update css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-//client/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Resume from './pages/Resume';
@@ -19,6 +19,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
